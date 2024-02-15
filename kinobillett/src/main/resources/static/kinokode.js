294fb24918bd3bca42cc0abe8e683f1e661978d9
angeclara <141249648+angeclara@users.noreply.github.com>
2024-02-15 22:19:41 +0100

    begynt med koden, koblet js til html fil

---
+++ kinobillett/src/main/resources/static/kinokode.js
@@ -0,0 +1,6 @@
+let kunde = {
+    fornavn : document.getElementById(fornavn),
+    etternavn : document.getElementById(etternavn),
+    telefonnummer: document.getElementById(number),
+    epost: document.getElementById(email),
+}
