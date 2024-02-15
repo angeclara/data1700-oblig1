0c080f1bf903f5d5f6af444eed7bdb4041fe1b89
angeclara <141249648+angeclara@users.noreply.github.com>
2024-02-15 18:58:10 +0100

    created html

---
+++ kinobillett/src/main/java/com/example/kinobillett/KinobillettApplication.java
@@ -0,0 +1,13 @@
+package com.example.kinobillett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinobillettApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinobillettApplication.class, args);
+    }
+
+}
