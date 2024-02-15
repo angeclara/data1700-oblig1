2331a4d286bb075905f3d0527bf4c81d0a976d90
angeclara <141249648+angeclara@users.noreply.github.com>
2024-02-15 23:52:08 +0100

    jobber med koden

--- kinobillett/src/main/resources/static/kinokode.js
+++ kinobillett/src/main/resources/static/kinokode.js
@@ -1,6 +1,17 @@
-let kunde = {
-    fornavn : document.getElementById(fornavn),
-    etternavn : document.getElementById(etternavn),
-    telefonnummer: document.getElementById(number),
-    epost: document.getElementById(email),
+let antall = document.getElementById(antall);
+let i = 0;
+function billett(film, fornavn, etternavn, telefonnr, email) {
+    this.film = film;
+    this.fornavn = fornavn;
+    this.etternavn = etternavn;
+    this.telefonnr = telefonnr;
+    this.email = email;
+}
+const kunde = [];
+function kjøp() {
+    kunde[i] = billett(document.getElementById("film").value,
+        document.getElementById("fornavn").value,
+        document.getElementById("etternavn").value,
+        document.getElementById(number).value,
+        document.getElementById("email").value,);
 }
